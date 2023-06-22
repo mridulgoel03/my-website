4df2f0a67b47ff7733aeb0f376c3c062b35ccec1
Mridul Goel <goel.mridul555@gmail.com>
2023-06-22 21:57:17 +0530

    .

---
+++ src/pages/Desktop1.js
@@ -0,0 +1,70 @@
+import styles from "./Desktop1.module.css";
+const Desktop1 = () => {
+  return (
+    <div className={styles.desktop1}>
+      <img className={styles.icon} alt="" src="/1-1@2x.png" />
+      <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
+      <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
+      <img className={styles.image3Icon} alt="" src="/image-3@2x.png" />
+      <img className={styles.image4Icon} alt="" src="/image-4@2x.png" />
+      <img className={styles.image5Icon} alt="" src="/image-5@2x.png" />
+      <div
+        className={styles.sanskarPrintingPress}
+      >{`Sanskar Printing Press `}</div>
+      <img className={styles.image8Icon} alt="" src="/image-8@2x.png" />
+      <a
+        className={styles.buyNow}
+        href="https://wa.me/9826167521"
+        target="_blank"
+      >
+        Buy Now
+      </a>
+      <a className={styles.callNow} href="tel:+919826167521">
+        Call Now
+      </a>
+      <img className={styles.image11Icon} alt="" src="/image-11@2x.png" />
+      <img className={styles.image3Icon1} alt="" src="/image-31@2x.png" />
+      <img className={styles.image4Icon1} alt="" src="/image-41@2x.png" />
+      <img className={styles.desktop1Child} alt="" src="/ellipse-9@2x.png" />
+      <img className={styles.desktop1Item} alt="" src="/ellipse-10@2x.png" />
+      <img className={styles.desktop1Inner} alt="" src="/ellipse-7@2x.png" />
+      <img className={styles.image12Icon} alt="" src="/image-12@2x.png" />
+      <b className={styles.youHaveToContainer}>
+        <p className={styles.youHaveTo}>
+          YOU HAVE TO GIVE ODER DY THE HELP OF WHATSAPP
+        </p>
+        <p className={styles.youHaveTo}> OR CALL</p>
+      </b>
+      <b className={styles.shahidSmarakPath}>
+        Shahid smarak path, Kachhiyana Rd, Jabalpur, Madhya Pradesh 482002
+      </b>
+      <b className={styles.shahidSmarakPath1}>
+        Shahid smarak path, Kachhiyana Rd, Jabalpur, Madhya Pradesh 482002
+      </b>
+      <div className={styles.madeBySanskar}>made by Sanskar Printing Press</div>
+      <a className={styles.a} href="tel:+919826167521">
+        +91 9826167521
+      </a>
+      <a className={styles.a1} href="tel:+919826167521">
+        +91 7803963439
+      </a>
+      <div className={styles.menuCard}>
+        <p className={styles.youHaveTo}>{`Menu `}</p>
+        <p className={styles.youHaveTo}>Card</p>
+      </div>
+      <div className={styles.pamphlet}>Pamphlet</div>
+      <div className={styles.shadiCard}>
+        <p className={styles.youHaveTo}>Shadi</p>
+        <p className={styles.youHaveTo}> card</p>
+      </div>
+      <div className={styles.birthdayCard}>Birthday card</div>
+      <div className={styles.idCard}> ID card</div>
+      <div className={styles.latterPad}>
+        <p className={styles.youHaveTo}>Latter</p>
+        <p className={styles.youHaveTo}>pad</p>
+      </div>
+    </div>
+  );
+};
+
+export default Desktop1;
